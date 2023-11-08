d169f11ca03d0913ecfa725f2302e55b1eca64d9
ryota0267 <ryota.akechi@gmail.com>
2023-11-08 04:17:45 +0000

    Implementation of like and comment functions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :index, :edit, :update]
   get "home/about", to: "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
